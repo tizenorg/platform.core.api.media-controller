2ad713b0f00ea549b341fbb0ce67d8bc9d4d2cf6
Ji Yong Min <jiyong.min@samsung.com>
2015-07-09 20:31:30 +0900

    Modify media controller not working
    - Often IPC connection disconnected
    - DB query fail because db path of uid was wrong
    - Add to wait for service activation & do not connect when service is already running
    
    Change-Id: I5db05d250f30151b52371bb9b5d10c8179f77862
    Signed-off-by: Jiyong Min <jiyong.min@samsung.com>

--- packaging/media-controller_create_db.sh
+++ packaging/media-controller_create_db.sh
@@ -20,5 +20,5 @@ chmod 644 ${TZ_USER_DB}/.media_controller.db-journal
 
 #Change group (6017: db_filemanager 5000: app)
 chgrp $TZ_SYS_USER_GROUP ${TZ_USER_DB}
-chgrp 6017 ${TZ_USER_DB}/.media_controller.db
-chgrp 6017 ${TZ_USER_DB}/.media_controller.db-journal
+#chgrp 6017 ${TZ_USER_DB}/.media_controller.db
+#chgrp 6017 ${TZ_USER_DB}/.media_controller.db-journal
